9b3f6584471d9fae28b47baaed602dee138ec36f
o-maklecovs <p2528743@my365.dmu.ac.uk>
2022-07-13 19:45:40 +0100

    Added styling to to-dos list section

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,17 @@
 import Button from './Button';
+import { FaTimes, FaEdit } from 'react-icons/fa';
 
 const Todo = ({ todo, onClickShowEditTodo, onClickTodoCheck, onClickTodoDelete }) => {
   return (
     <div className="todo">
-      <input type="checkbox" name="isChecked" checked={todo.isChecked} onChange={e => onClickTodoCheck(todo.id, e.target.checked)} />
-      <label htmlFor="isChecked">{todo.task}</label>
-      <Button type='' text='Edit' onClick={() => onClickShowEditTodo(todo.id)} />
-      <Button type='' text='Delete' onClick={() => onClickTodoDelete(todo.id)} />
+      <div>
+        <input type="checkbox" name="isChecked" checked={todo.isChecked} onChange={e => onClickTodoCheck(todo.id, e.target.checked)} />
+        <label htmlFor="isChecked" className={todo.isChecked ? 'checked' : ''}>{todo.task}</label>
+      </div>
+      <div>
+        <FaEdit className="btn-edit" onClick={() => onClickShowEditTodo(todo.id)} />
+        <FaTimes className="btn-del" onClick={() => onClickTodoDelete(todo.id)} />
+      </div>
     </div>
   );
 };
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,11 @@
 import Button from './Button';
+import { FaPlus } from 'react-icons/fa';
 import Todo from './Todo';
 
 const Todos = ({ todos, onClickShowAddTodo, onClickShowEditTodo, onClickTodoCheck, onClickTodoDelete }) => {
   return (
     <main>
-      <Button type='' text='Add' onClick={onClickShowAddTodo} />
+      <FaPlus className="btn-sqr" onClick={onClickShowAddTodo} />
       <div className="todos">
         {todos.length > 0 ? todos.map(todo => <Todo key={todo.id} todo={todo} onClickShowEditTodo={onClickShowEditTodo} onClickTodoCheck={onClickTodoCheck} onClickTodoDelete={onClickTodoDelete} />) : <p className="empty">Empty</p>}
       </div>
